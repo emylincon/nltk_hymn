fe688fb0000e301aad1f5b7ff51eaf0d9d0142da
Emeka Ugwuanyi <emylincon@gmail.com>
2019-04-07 19:19:26 +0100

    creating synomyns function
    Signed-off-by: Emeka Ugwuanyi <emylincon@gmail.com>

--- hymn_sim.py
+++ hymn_sim.py
@@ -1,17 +1,69 @@
-from nltk.tokenize import sent_tokenize
 from nltk.corpus import wordnet
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
+from nltk.stem import PorterStemmer
 
-'''
-s = open("C:/Users/emyli/PycharmProjects/nltk_song/hymns/amazing_grace", 'r').read()
-tok = sent_tokenize(s)
-t = s.split()
-print(t[1])
-'''
+ps = PorterStemmer()
 
-symn = wordnet.synsets('love')
-li = []
-#print(symn[2].lemmas()[0].name())
+stop_words = set(stopwords.words("english"))
 
-for i in symn:
-    li.append(symn[i].lemmas()[0].name())
-print(set(li))
+
+def read_file():
+    s = open("C:/Users/emyli/PycharmProjects/nltk_song/hymns/amazing_grace", 'r').read()
+    return s
+
+
+def remove_stopwords():
+    enter_words = read_file()
+    words = word_tokenize(enter_words)
+    #stemmed = [ps.stem(w) for w in words]
+    filt_sen = [w for w in words if not w in stop_words]
+    good = [w for w in filt_sen if len(w)>2]
+    better = [i for i in [w.lower() for w in good] if i!='the' and i!='and']
+    return better
+
+
+def find_symn(word):
+    symn = wordnet.synsets(word)
+    li = []
+    #print(symn[2].lemmas()[0].name())
+
+    for i in range(len(symn)):
+        li.append(symn[i].lemmas()[0].name())
+    return set(li)
+
+
+def word_similarity(word1, word2):
+    w1 = wordnet.synset("{}.v.01".format(word1))
+    w2 = wordnet.synset("{}.v.01".format(word2))
+    return w1.wup_similarity(w2)
+
+
+def word_freq(words):
+    words = nltk.FreqDist(words)
+    print(words.keys())
+    print(words.most_common(15))
+    print(words.max())
+    words.plot(15)
+
+
+# find_symn('love')
+# print(word_similarity('love', 'like'))
+# word_freq(remove_stopwords())
+# print(remove_stopwords())
+
+def hymn():
+    sym = find_symn('love')
+    hymn = nltk.FreqDist(remove_stopwords())
+    for i in sym:
+        if i in hymn.keys():
+            print("{}: {}".format(i, hymn.freq(i)))
+        else:
+            print('no match for ', i)
+    h = hymn['love']
+    print(h)
+    hymn.plot(10)
+
+
+print(remove_stopwords())
