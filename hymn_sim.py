4fde033762bae89fdfb37c09f42aa156303d77b4
Emeka Ugwuanyi <emylincon@gmail.com>
2019-04-07 16:12:37 +0100

    creating synomyns function
    Signed-off-by: Emeka Ugwuanyi <emylincon@gmail.com>

--- hymn_sim.py
+++ hymn_sim.py
@@ -1,7 +1,17 @@
 from nltk.tokenize import sent_tokenize
 from nltk.corpus import wordnet
 
+'''
 s = open("C:/Users/emyli/PycharmProjects/nltk_song/hymns/amazing_grace", 'r').read()
 tok = sent_tokenize(s)
 t = s.split()
 print(t[1])
+'''
+
+symn = wordnet.synsets('love')
+li = []
+#print(symn[2].lemmas()[0].name())
+
+for i in symn:
+    li.append(symn[i].lemmas()[0].name())
+print(set(li))
